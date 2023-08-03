e59bae4f7f5751160d82434bd85b23d5d1869611
Akhmadali <akhmadalibakhtiyarov@mail.ru>
2023-08-03 11:46:53 +0500

    update: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let b = n.toString()
+    let c = b.split("")
+
+    if(c.includes("-")){
+        return Number(c.slice(1).reverse().join(""))
+    }else{
+        return Number(c.reverse().join(""))
+    }
 }
